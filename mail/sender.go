49817ff0024a3e69b0fb40b91f4e76c9acc36e37
Veron Baranige <veron.b@hsenidoutsourcing.com>
2024-03-07 13:36:31 +0530

    Refactor mail sending

--- mail/sender.go
+++ mail/sender.go
@@ -3,10 +3,24 @@ package mail
 import (
 	"crypto/tls"
 
+	"github.com/veron-baranige/ssl-reminder/config"
 	"gopkg.in/gomail.v2"
 )
 
-func SendMail(mailMessage *gomail.Message) error {
+func SendMail(subject string, content string) error {
+	mailDialer := gomail.NewDialer(
+		config.SmtpHost,
+		config.SmtpPort,
+		config.SmtpUser, 
+		config.SmtpPassword,
+	)
+
+	message := gomail.NewMessage()
+	message.SetHeader("From", config.SmtpUser)
+	message.SetHeader("To", config.ReceiverMailAddresses...)
+	message.SetHeader("Subject", subject)
+	message.SetBody("text/plain", content)
+	
 	mailDialer.TLSConfig = &tls.Config{InsecureSkipVerify: true}
-	return mailDialer.DialAndSend(mailMessage)
+	return mailDialer.DialAndSend(message)
 }
